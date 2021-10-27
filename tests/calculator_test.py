37388fed52c9095c289b2de329fd7bc7562e23a0
k3vinhu4ng <kh35@njit.edu>
2021-10-27 19:53:32 -0400

    added test fixed coverage

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,10 +1,18 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
+from calculator.main import Calculator, inc
+
+
+def test_inc():
+    """testing inc function"""
+    x_value = 5
+    assert inc(x_value) == 6
+
 
 def test_calculator_result():
     """testing calculator result is 0"""
     calc = Calculator()
     assert calc.result == 0
+
 
 def test_calculator_add():
     """Testing the Add function of the calculator"""
@@ -15,17 +23,20 @@ def test_calculator_add():
     # Assert that the results are correct
     assert calc.result == 1
 
+
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     calc.add_number(1)
     assert calc.get_result() == 1
 
+
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
 
 def test_calculator_multiply():
     """Testing the multiply method of the calculator"""
@@ -41,6 +52,7 @@ def test_calculator_divide():
     calc.result = 50
     calc.divide_number(2)
     assert calc.get_result() == 25
+
 
 def test_calculator_divide_by_zero():
     """Testing the divide method of the calculator dividing by zero"""

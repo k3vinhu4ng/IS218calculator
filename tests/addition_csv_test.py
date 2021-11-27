f27bd63582b26452db3680bb895268f0f024ab6b
k3vinhu4ng <kh35@njit.edu>
2021-11-27 00:04:28 -0500

    added arrange act assert

--- tests/addition_csv_test.py
+++ tests/addition_csv_test.py
@@ -1,12 +1,22 @@
 """Test addition csv"""
-import unittest
 from calculator.calculations.addition import Addition
 from calculator.csv_reader.reader import CsvReader
 
+
 def addition_csv_test():
-    test_file = CsvReader("tests/tests_data/addition.csv")
+
+    #Arrange
+    test_file = CsvReader("tests/tests_data/addition.csv").data
+
     for i in test_file:
         my_tuple = (i[0])
         result = i[1]
+        #Act
         addition = Addition(my_tuple)
-        assert addition.get() == result
+
+        #Assert
+        assert addition.get_result() == result
+
+
+
+
